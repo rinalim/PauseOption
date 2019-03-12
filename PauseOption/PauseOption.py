7a2d6c4ed4706db81cfbbb8a5ad83d9517ed6ca3
rinalim <46272802+rinalim@users.noreply.github.com>
2019-03-13 01:55:18 +0900

    Update PauseOption.py

--- PauseOption/PauseOption.py
+++ PauseOption/PauseOption.py
@@ -4,39 +4,71 @@ import xml.etree.ElementTree as ET
 #CONFIG = '/opt/retropie/configs/'
 CONFIG = '/home/csle/PauseOptionDev/configs/'
 
+user_key = {}
 btn_map = {}
+es_conf = 1
 
+def load_layout():
 
+    #print ' -(1)-----  -(2)-----  -(3)----- '
+    #print ' | X Y L |  | Y X L |  | L Y X | '
+    #print ' | A B R |  | B A R |  | R B A | '
+    #print ' ---------  ---------  --------- '
+
+    if es_conf == 1:
+        user_key['1'] = 'x'
+        user_key['2'] = 'y'
+        user_key['3'] = 'l'
+        user_key['4'] = 'a'
+        user_key['5'] = 'b'
+        user_key['6'] = 'r'
+    elif es_conf == 2:
+        user_key['1'] = 'y'
+        user_key['2'] = 'x'
+        user_key['3'] = 'l'
+        user_key['4'] = 'b'
+        user_key['5'] = 'a'
+        user_key['6'] = 'r'
+    elif es_conf == 3:
+        user_key['1'] = 'l'
+        user_key['2'] = 'y'
+        user_key['3'] = 'x'
+        user_key['4'] = 'r'
+        user_key['5'] = 'b'
+        user_key['6'] = 'a'
+
+            
 def get_info(romname):
 
-    #INPUT = './controls.xml'
-    
+    #INPUT = './controls.xml'   
     if os.path.isfile('./xml/'+romname+'.xml') == False:
         print 'No Game Found'
-        return 'NoGame'
-    doc = ET.parse('./xml/'+romname+'.xml')
-    game = doc.getroot()
-    #game = root.find('./game[@romname=\"' + romname + '\"]')
-    if game == None:
-       print 'No Game Found'
+        name = romname
+        lever = '0'
+        buttons = ['Button A', 'Button B', 'Button C', 'Button D']
     else:
-       name = game.get('gamename') 
-       print name
-       player = game.find('player')
-       controls = player.find('controls')
-       lever = controls[0].get('name')
-       print lever
-       labels = player.findall('labels')
-       buttons = []
-       for i in labels[0]:
-           if 'BUTTON' in i.get('name'):
-               buttons.append(i.get('value'))
-               print i.get('name'), i.get('value')
+        doc = ET.parse('./xml/'+romname+'.xml')
+        game = doc.getroot()
+    #game = root.find('./game[@romname=\"' + romname + '\"]')
+    #if game == None:
+    #   print 'No Game Found'
+        name = game.get('gamename') 
+        print name
+        player = game.find('player')
+        controls = player.find('controls')
+        lever = controls[0].get('name')
+        print lever
+        labels = player.findall('labels')
+        buttons = []
+        for i in labels[0]:
+            if 'BUTTON' in i.get('name'):
+                buttons.append(i.get('value'))
+                print i.get('name'), i.get('value')
     
     return name, lever, buttons
 
 
-def get_btn_layout(emul, romname):
+def get_btn_layout(emul, romname, buttons):
 
     f = open('/tmp/js.log', 'r')
     line = f.readline()
@@ -46,6 +78,7 @@ def get_btn_layout(emul, romname):
     f.close()
 
     print 'Load global setting'
+    '''
     f = open(CONFIG + 'all/retroarch/autoconfig/' + dev_name + '.cfg', 'r')
     #f = open("/home/csle/PauseOptionDev/configs/fba/FB Alpha/mslug.rmp", 'r')
     while True:
@@ -63,6 +96,15 @@ def get_btn_layout(emul, romname):
         if len(words[0]) == 1:    # input_a_btn = "0"
             btn_map[words[0]] = words[1]   
     f.close()
+    '''
+    
+    # FBA button sequence = [0, 8, 1, 9, 10, 11]   
+    btn_map['a'] = '"0"'
+    btn_map['b'] = '"8"'
+    btn_map['x'] = '"1"'
+    btn_map['y'] = '"9"'
+    btn_map['l'] = '"10"'
+    btn_map['r'] = '"11"'
 
     if os.path.isfile(CONFIG + 'fba/FB Alpha/FB Alpha.rmp') == True:
         print 'Override with emulator setting'
@@ -101,13 +143,24 @@ def get_btn_layout(emul, romname):
         f.close()
 
     print btn_map
-        
-        
-        #retroarch_key[words[0]] = words[2].replace('"','')
-       
-    f.close()
-    
+    # Convert from the FBA sequence to the normal sequence
+    convert = {}
+    convert['"0"'] = 0
+    convert['"8"'] = 1
+    convert['"1"'] = 2
+    convert['"9"'] = 3
+    convert['"10"'] = 4
+    convert['"11"'] = 5
+    btn_map['a'] = convert[btn_map['a']]
+    btn_map['b'] = convert[btn_map['b']]
+    btn_map['x'] = convert[btn_map['x']]
+    btn_map['y'] = convert[btn_map['y']]
+    btn_map['l'] = convert[btn_map['l']]
+    btn_map['r'] = convert[btn_map['r']]   
+    print btn_map
 
+    # Do capcom patch here
+    
 def draw_picture(romname, name, lever, buttons):
 
     RESUME = " ./result/" + romname + "_resume.png"
@@ -154,7 +207,7 @@ def draw_picture(romname, name, lever, buttons):
         cmd = "composite -geometry 70x70+36+260 ./img/" + lever[0] + "way.png" + RESUME + RESUME
         os.system(cmd)
 
-    get_btn_layout('fba', romname)
+    get_btn_layout('fba', romname, buttons)
     
     # Configured button layout
     pos = ["80x18+124+270", "80x18+207+270", "80x18+290+270", "80x18+124+300", "80x18+207+300", "80x18+290+300"]
@@ -183,13 +236,9 @@ def main():
         words = line.split()
         f.close()
         if len(words) >= 2:
-            if os.path.isfile('./xml/'+words[1]+'.xml') == False:
-                print 'No Game Found'
-                os.system("cp ./img/bg_resume.png ./resume.png")
-            else:
-                name, lever, buttons = get_info(words[1])
-                #print name, lever, buttons
-                draw_picture(words[1], name, lever, buttons)
+            name, lever, buttons = get_info(words[1])
+            #print name, lever, buttons
+            draw_picture(words[1], name, lever, buttons)
 
 
 if __name__ == "__main__":
