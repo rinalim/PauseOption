e776942066826538a955687765eef744fef9de9c
rinalim <46272802+rinalim@users.noreply.github.com>
2019-03-14 17:40:20 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ echo 'rom_name=$3' >> /opt/retropie/configs/all/runcommand-onstart.sh
 echo 'rom_name="${rom_name##*/}"' >> /opt/retropie/configs/all/runcommand-onstart.sh 
 echo 'rom_name="${rom_name%.*}"' >> /opt/retropie/configs/all/runcommand-onstart.sh 
 echo 'echo "$1 $rom_name" > /tmp/PauseOption.log' >> /opt/retropie/configs/all/runcommand-onstart.sh
-#echo '/usr/bin/python /opt/retropie/configs/all/PauseOption/PauseOption.py &' >> /opt/retropie/configs/all/runcommand-onstart.sh
+echo '/usr/bin/python /opt/retropie/configs/all/PauseOption/PauseOption.py &' >> /opt/retropie/configs/all/runcommand-onstart.sh
 
 cd /home/pi/PauseOption/PauseOption/PauseMode
 sudo chmod 755 update.sh
