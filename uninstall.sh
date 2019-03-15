491df1e5c118368b2f7e3763cc6039cc3794112c
rinalim <46272802+rinalim@users.noreply.github.com>
2019-03-15 17:10:55 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@ rm -rf /opt/retropie/configs/all/PauseOption/
 
 sudo sed -i '/PauseOption/d' /opt/retropie/configs/all/runcommand-onstart.sh 
 
-cp ./backup/*png /opt/retropie/configs/all/PauseMode/
+cp /home/pi/PauseMode/PauseMode/*png /opt/retropie/configs/all/PauseMode/
 
 echo
 echo "Uninstall Completed"
